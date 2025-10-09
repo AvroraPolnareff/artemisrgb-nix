6f1e8315640855bac5161caaf7e012c9d2bf511e
Prunkles <prunkles@pawuq.net>
2025-10-10 00:42:29 +0300

    artemisrgb: Add `builtinPlugins` customisation

--- packages/artemisrgb/default.nix
+++ packages/artemisrgb/default.nix
@@ -2,22 +2,36 @@
   lib,
   runCommand,
   makeWrapper,
-
-  artemisrgb-unwrapped,
-  artemisrgb-plugins,
+  symlinkJoin,
 }:
+artemisrgb-unwrapped:
 let
   inherit (lib) getExe;
+  wrapper =
+    {
+      builtinPlugins ? [ ],
+    }:
+    let
+      builtinPluginsDir = symlinkJoin {
+        name = "artemisrgb-plugins";
+        paths = builtinPlugins;
+      };
+    in
+    runCommand "artemisrgb-${artemisrgb-unwrapped.version}"
+      {
+        inherit (artemisrgb-unwrapped) version meta;
+        pname = "artemisrgb";
+        nativeBuildInputs = [ makeWrapper ];
+        passthru = {
+          unwrapped = artemisrgb-unwrapped;
+          inherit builtinPlugins;
+        };
+      }
+      ''
+        mkdir -p "$out/bin"
+        ln -s "${artemisrgb-unwrapped}/share" "$out/share"
+        makeWrapper "${getExe artemisrgb-unwrapped}" "$out/bin/artemisrgb" \
+          --set ARTEMISRGB_BUILTIN_PLUGIN_DIR ${builtinPluginsDir}
+      '';
 in
-runCommand "artemisrgb-${artemisrgb-unwrapped.version}"
-  {
-    inherit (artemisrgb-unwrapped) version meta;
-    pname = "artemisrgb";
-    nativeBuildInputs = [ makeWrapper ];
-  }
-  ''
-    mkdir -p "$out/bin"
-    ln -s "${artemisrgb-unwrapped}/share" "$out/share"
-    makeWrapper "${getExe artemisrgb-unwrapped}" "$out/bin/artemisrgb" \
-      --set ARTEMISRGB_BUILTIN_PLUGIN_DIR ${artemisrgb-plugins}
-  ''
+lib.makeOverridable wrapper
