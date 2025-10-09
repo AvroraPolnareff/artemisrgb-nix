611e214fbe55273adb7e5b61ca1134dea7a1db23
AvroraPolnareff <araintheskywith@gmail.com>
2025-10-09 04:39:10 +0300

    desktopItem

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
   outputs = { self, nixpkgs, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
         pluginPaths = [
           "./src/Collections/Artemis.Plugins.Audio"
           "./src/Collections/Artemis.Plugins.Input"
@@ -45,55 +47,71 @@
           "./src/Nodes/Artemis.Plugins.Nodes.General"
           "./src/Other/Artemis.Plugins.Profiling"
         ];
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
-        artemisSrc = pkgs.fetchFromGitHub {
-          owner = "Artemis-RGB";
-          repo = "Artemis";
-          rev = "acd35176e1c6661a887e467ef4b294eddb532726";
-          hash = "sha256-ufYe3Nxcqy0SeXyfpgsUlvSFUj6G/l/0eOSuuu3eJ5E=";
-        };
-        artemisPluginsSrc = pkgs.fetchFromGitHub {
-          owner = "Artemis-RGB";
-          repo = "Artemis.Plugins";
-          rev = "5d2fe3d028c24b5cc78fc748b431ed1b984b3cd4";
-          hash = "sha256-IX5ZiElbg/wKpMQ1mDEl7Dktp7PqCfrksIgXp057LVk=";
-        };
         dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
         dotnet-runtime = pkgs.dotnetCorePackages.runtime_9_0;
-        artemisrgb-unwrapped = pkgs.buildDotnetModule {
+        artemisrgb-unwrapped = pkgs.buildDotnetModule (finalAttrs: {
           inherit dotnet-sdk dotnet-runtime;
           pname = "artemisrgb-unwrapped";
           version = "2.0.0";
-          src = artemisSrc;
+          src = pkgs.fetchFromGitHub {
+            owner = "Artemis-RGB";
+            repo = "Artemis";
+            rev = "acd35176e1c6661a887e467ef4b294eddb532726";
+            hash = "sha256-ufYe3Nxcqy0SeXyfpgsUlvSFUj6G/l/0eOSuuu3eJ5E=";
+          };
           projectFile = "./src/Artemis.UI.Linux/Artemis.UI.Linux.csproj";
           nugetDeps = ./artemis.deps.json;
           executables = [ "Artemis.UI.Linux" ];
           patchPhase = ''
+            # for some reason original constant doesn't changes at runtime so we replace it to an env variable
             substituteInPlace ./src/Artemis.Core/Services/PluginManagementService.cs \
               --replace-fail 'DirectoryInfo builtInPluginDirectory = new(Path.Combine(Constants.ApplicationFolder, "Plugins"));' \
               'DirectoryInfo builtInPluginDirectory = new(Environment.GetEnvironmentVariable("ARTEMISRGB_BUILTIN_PLUGIN_DIR") ?? Path.Combine(Constants.ApplicationFolder, "Plugins"));'
           '';
+          postInstall = ''
+            mkdir -p "$out/share/icons"
+            ln -s "$out/lib/${finalAttrs.pname}/Icons" "$out/share/icons/hicolor"
+          '';
           postFixup = ''
             mv $out/bin/Artemis.UI.Linux $out/bin/artemisrgb
           '';
+          nativeBuildInputs = [
+            pkgs.copyDesktopItems
+          ];
+          desktopItems = [
+            (pkgs.makeDesktopItem {
+              name = "artemisrgb";
+              desktopName = "Artemis RGB";
+              icon = "artemis";
+              exec = "artemisrgb"; 
+              categories = [ "Utility" ];
+              keywords = [ "artemis" "rgb" "lighting" "software" ];
+            })
+          ];
           meta = with pkgs.lib; {
             description = "Artemis";
             mainProgram = "artemisrgb";
             platforms = platforms.linux;
           };
-        };
+        });
         artemisrgb-plugins = pkgs.buildDotnetModule {
           inherit dotnet-sdk dotnet-runtime;
           inherit pluginPaths; 
           pname = "artemisrgb-plugins";
           version = "2.0.0";
-          src = artemisPluginsSrc;
+          src = pkgs.fetchFromGitHub {
+            owner = "Artemis-RGB";
+            repo = "Artemis.Plugins";
+            rev = "5d2fe3d028c24b5cc78fc748b431ed1b984b3cd4";
+            hash = "sha256-IX5ZiElbg/wKpMQ1mDEl7Dktp7PqCfrksIgXp057LVk=";
+          };
           projectFile = "./src/Artemis.Plugins.sln";
           nugetDeps = ./artemis.plugins.deps.json;
           dotnetRestoreFlags = ["--artifacts-path ./artifacts"];
           dotnetBuildFlags = ["--artifacts-path ./artifacts"];
           patchPhase = ''
+            # for ease of use Artemis.Plugins has a references to local copy of artemis repo
+            # but in order to build these plugins in nix we want to use paths to our unwrapped package in nix store
             substituteInPlace ./src/Directory.Build.props \
               --replace-fail '..\..\..\..\Artemis\src\Artemis.Core\bin\net9.0\Artemis.Core.dll' \
               '${artemisrgb-unwrapped}/lib/artemisrgb-unwrapped/Artemis.Core.dll'            
@@ -114,7 +132,6 @@
           ];
           installPhase = ''
             runHook preInstall
-            
             mkdir -p $out
             for pluginPath in $pluginPaths; do
               pluginName="$(basename "$pluginPath")"
@@ -129,13 +146,15 @@
             platforms = platforms.linux;
           };
         };
-        artemisrgb = pkgs.writeShellApplication {
-          name = "artemisrgb-${artemisrgb-unwrapped.version}";
-          text = ''
-            export ARTEMISRGB_BUILTIN_PLUGIN_DIR=${artemisrgb-plugins}
-            exec ${lib.getExe artemisrgb-unwrapped}
-          '';
-        };
+        artemisrgb = pkgs.runCommand "artemisrgb-${artemisrgb-unwrapped.version}" {
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+        }
+        ''
+          mkdir -p "$out/bin"
+          ln -s "${artemisrgb-unwrapped}/share" "$out/share"
+          makeWrapper "${lib.getExe artemisrgb-unwrapped}" "$out/bin/artemisrgb" \
+            --set ARTEMISRGB_BUILTIN_PLUGIN_DIR ${artemisrgb-plugins}
+        '';
       in
       {
         packages.artemisrgb-unwrapped = artemisrgb-unwrapped;
